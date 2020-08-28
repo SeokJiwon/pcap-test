df09d7e7010083266ea6f6021bd0b60522ddea8d
SeokJiwon <tjrwldnjs103@naver.com>
2020-08-29 06:35:44 +0900

    Done except payload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,61 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#define eth_len 14
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+    /* Ethernet header */
+    struct sniff_ethernet {
+        u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+        u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+        u_short ether_type; /* IP? ARP? RARP? etc */
+    };
+
+    /* IP header */
+    struct sniff_ip {
+        u_char ip_v:4;/* version << 4 | header length >> 2 */
+        u_char ip_ihl:4;
+        u_char ip_tos;		/* type of service */
+        u_short ip_len;		/* total length */
+        u_short ip_id;		/* identification */
+        u_short ip_off;		/* fragment offset field */
+    #define IP_RF 0x8000		/* reserved fragment flag */
+    #define IP_DF 0x4000		/* dont fragment flag */
+    #define IP_MF 0x2000		/* more fragments flag */
+    #define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+        u_char ip_ttl;		/* time to live */
+        u_char ip_p;		/* protocol */
+        u_short ip_sum;		/* checksum */
+        struct in_addr ip_src,ip_dst; /* source and dest address */
+    };
+
+    /* TCP header */
+    typedef u_int tcp_seq;
+
+    struct sniff_tcp {
+        u_short th_sport;	/* source port */
+        u_short th_dport;	/* destination port */
+        tcp_seq th_seq;		/* sequence number */
+        tcp_seq th_ack;		/* acknowledgement number */
+        u_char th_off:4;	/* data offset */
+        u_char th_rsvd:4;
+        u_char th_flags;
+    #define TH_FIN 0x01
+    #define TH_SYN 0x02
+    #define TH_RST 0x04
+    #define TH_PUSH 0x08
+    #define TH_ACK 0x10
+    #define TH_URG 0x20
+    #define TH_ECE 0x40
+    #define TH_CWR 0x80
+    #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;		/* window */
+        u_short th_sum;		/* checksum */
+        u_short th_urp;		/* urgent pointer */
+};
 
 void usage() {
     printf("syntax: pcap-test <interface>\n");
@@ -11,6 +67,11 @@ int main(int argc, char* argv[]) {
         usage();
         return -1;
     }
+
+    struct sniff_ethernet *eth;
+    struct sniff_ip *ip;
+    struct sniff_tcp *tcp;
+    char* payload;
 
     char* dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -29,7 +90,20 @@ int main(int argc, char* argv[]) {
             printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
             break;
         }
-        printf("%u bytes captured\n", header->caplen);
+        eth = (struct sniff_ethernet *)packet;
+        ip = (struct sniff_ip *)(packet+eth_len);
+        int ip_size=ip->ip_ihl*4;
+        tcp= (struct sniff_tcp *)(packet+eth_len+ip_size);
+        int tcp_size=tcp->th_off;
+        payload=(char*)(packet+eth_len+ip_size+tcp_size);
+        int data_len=ip->ip_len-tcp_size-ip_size;
+        printf("Source Mac: %02x:%02x:%02x:%02x:%02x:%02x -> Destination Mac: %02x:%02x:%02x:%02x:%02x:%02x\n",eth->ether_shost[0],eth->ether_shost[1],eth->ether_shost[2],eth->ether_shost[3],eth->ether_shost[4],eth->ether_shost[5],eth->ether_dhost[0],eth->ether_dhost[1],eth->ether_dhost[2],eth->ether_dhost[3],eth->ether_dhost[4],eth->ether_dhost[5]);
+        printf("Source IP: %s:%d -> Destination IP: %s:%d\n", inet_ntoa(ip->ip_src),ntohs(tcp->th_sport),inet_ntoa(ip->ip_dst),ntohs(tcp->th_dport));
+        (data_len>16 ? data_len=16 : data_len=data_len);
+        for(int i=0;i<data_len;i++){
+            printf("%s", payload[i]);
+        }
+        printf("\n========================================\n");
     }
 
     pcap_close(handle);
